6ecbbf8ca055bc53c4c7a4bc94a324a3dc372f53
RamKarthik-20 <rkn.20307@gmail.com>
2026-03-24 15:33:25 +0530

    UC7

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,92 +1,23 @@
 public class BookMyStayApp {
-    import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+    class AddOnService {
+        private String name;
+        private double price;
 
-    class BookingService {
-
-        private RoomInventory inventory;
-        private BookingQueue bookingQueue;
-
-        // Map: RoomType -> Set of allocated Room IDs
-        private Map<String, Set<String>> allocatedRooms;
-
-        // Global set to ensure uniqueness
-        private Set<String> allRoomIds;
-
-        private int idCounter = 1;
-
-        public BookingService(RoomInventory inventory, BookingQueue bookingQueue) {
-            this.inventory = inventory;
-            this.bookingQueue = bookingQueue;
-            this.allocatedRooms = new HashMap<>();
-            this.allRoomIds = new HashSet<>();
+        public AddOnService(String name, double price) {
+            this.name = name;
+            this.price = price;
         }
 
-        // Process all requests (FIFO)
-        public void processBookings() {
-            System.out.println("=== Processing Bookings ===");
-
-            Reservation request;
-
-            while ((request = bookingQueue.peekNext()) != null) {
-
-                // remove from queue
-                request = bookingQueue.pollRequest();
-
-                String type = request.getRoomType();
-                int available = inventory.getAvailability(type);
-
-                if (available > 0) {
-
-                    // Generate unique room ID
-                    String roomId = generateRoomId(type);
-
-                    // Ensure uniqueness
-                    while (allRoomIds.contains(roomId)) {
-                        roomId = generateRoomId(type);
-                    }
-
-                    // Store globally
-                    allRoomIds.add(roomId);
-
-                    // Store per room type
-                    allocatedRooms
-                            .computeIfAbsent(type, k -> new HashSet<>())
-                            .add(roomId);
-
-                    // Update inventory immediately
-                    inventory.updateAvailability(type, available - 1);
-
-                    // Confirm booking
-                    System.out.println("Booking Confirmed → "
-                            + request.getGuestName()
-                            + " | " + type
-                            + " | Room ID: " + roomId);
-
-                } else {
-                    System.out.println("Booking Failed (No Availability) → "
-                            + request.getGuestName()
-                            + " | " + type);
-                }
-            }
-
-            System.out.println("----------------------------");
+        public String getName() {
+            return name;
         }
 
-        // Generate ID
-        private String generateRoomId(String type) {
-            return type.replaceAll(" ", "").substring(0, 2).toUpperCase() + idCounter++;
+        public double getPrice() {
+            return price;
         }
 
-        // View allocations
-        public void displayAllocations() {
-            System.out.println("=== Allocated Rooms ===");
-            for (Map.Entry<String, Set<String>> entry : allocatedRooms.entrySet()) {
-                System.out.println(entry.getKey() + " -> " + entry.getValue());
-            }
-            System.out.println("------------------------");
+        public void display() {
+            System.out.println(name + " (₹" + price + ")");
         }
     }
 }
